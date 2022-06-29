d2e9cfb5ef4c02b1e56926829fa339b7fb1232e3
Precious Appiah <57904360+Awura-Precious@users.noreply.github.com>
2022-06-29 12:55:19 +0000

    refactor: project setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hi Isomorphic
+         </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
