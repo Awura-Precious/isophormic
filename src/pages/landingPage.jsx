5b8e6b929f5c2340a1da358942488055d7d5a350
Precious Appiah <57904360+Awura-Precious@users.noreply.github.com>
2022-06-30 17:52:48 +0000

    feat: gradient , css styles

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -1,42 +1,53 @@
+import {Link} from 'react-router-link';
+
 import Form  from 'react-bootstrap/Form'
 import Button  from 'react-bootstrap/Button'
 
 import groupImage from '../images/group2.jpg';
 import '../css/landingPage.css'
 
+
 const LandingPage =()=>{
   return (
-    <div className='big-div '>
-  
-        <img className="landing-image" src={groupImage} alt ="group of people"/>
+    <>
+
+    <div className='big-div mb-5'>
+        <div className="landing-image" >
+
+        </div>
 
         <div id='sec-div' className='d-grid'>
           <Form className='sign-in-form'>
-          <p className='text-center'>ISOMORPHIC</p>
+          <p className='text-center para'>ISOMORPHIC</p>
             <Form.Group>
               <Form.Control className= 'mb-3' size='md'  type='text' placeholder='Username'/>
             </Form.Group>
 
             <Form.Group>
-              <Form.Control size= 'md' className='mb-3' type='password' placeholder='Password'/>
+              <Form.Control size= 'md' className='mb-5' type='password' placeholder='Password'/>
             </Form.Group>
 
-            <Form.Group>
+            <Form.Group className='label-div mb-5' >
               <Form.Check label='Remember Me'/>
-              <Button>Sign In</Button>
+              <Button type='submit' variant ='custom'  id='signin-button' >Sign In</Button>
+              
             </Form.Group>
 
-            <div>
-              
+            <div className='buttons-div'>
+              <Button type ='submit' variant='custom' id='facebook'>sign in with Facebook</Button>
+              <Button type ='submit' variant='custom' id='google'>sign in with Google Plus</Button>
+              <Button type ='submit' variant='custom' id='auth' >sign in with Auth0</Button>
+              <Button  type ='submit' variant='custom' className='mb-5' id='firebase'  >sign in with Firebase</Button>
+              <Link className=' mb-2' id='first-link' href='/'>Forgot Password</Link>
+              <Link id='sec-link' href='/'>Create an Isomorphic account</Link>
             </div>
           </Form>
-           
-      
         </div>
-
-
-
     </div>
+
+    
+
+    </>
   );
 }
 
